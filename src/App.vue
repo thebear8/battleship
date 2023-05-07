7646cac8e1457ff7065b787618059bc385549f16
thebear8 <45793990+thebear8@users.noreply.github.com>
2023-05-07 17:14:39 +0000

    Simplify BattleshipGrid

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,27 @@
 <script setup lang="ts">
-import { ref } from "vue";
+import { reactive, ref } from "vue";
 import BattleshipGrid from "./components/BattleshipGrid.vue";
-import { type FieldState, type GameState } from "./state";
 
-const state = ref<GameState>({
-  selectedTile: null,
-  board: Array<FieldState[]>(10).fill(Array<FieldState>(10).fill("Empty")),
-});
+type FieldState = "Empty" | "Miss" | "Hit" | "Sunk";
+
+const selectedTile = ref<[number, number]>([-1, -1]);
+const board = reactive<FieldState[][]>(
+  Array<FieldState[]>(10).fill(Array<FieldState>(10).fill("Empty"))
+);
 </script>
 
 <template>
-  <BattleshipGrid :state="state" @select="(tile) => (state.selectedTile = tile)" />
+  <ChoiceDialog
+    v-if="selectedTile != null"
+    title="Tile type"
+    :options="[
+      ['Miss', () => (board[selectedTile[0]][selectedTile[1]] = 'Miss')],
+      ['Hit', () => (board[selectedTile[0]][selectedTile[1]] = 'Hit')],
+      ['Sunk', () => (board[selectedTile[0]][selectedTile[1]] = 'Sunk')],
+    ]"
+  />
+  <BattleshipGrid
+    :tiles="board.map((r) => r.map((f) => ({ color: `rgb(${76}, ${5}, ${25})` })))"
+    @click="(tile) => (selectedTile = tile)"
+  />
 </template>
--- src/components/BattleshipGrid.vue
+++ src/components/BattleshipGrid.vue
@@ -1,67 +1,35 @@
 <script setup lang="ts">
-import type { FieldState, GameState } from "../state";
-import ChoiceDialog from "./ChoiceDialog.vue";
-const props = defineProps<{ state: GameState }>();
-const emit = defineEmits<{
-  (event: "select", tile: [number, number]): any;
-  (event: "Miss", tile: [number, number]): any;
-  (event: "Hit", tile: [number, number]): any;
-  (event: "Sunk", tile: [number, number]): any;
+defineProps<{ tiles: { color: string }[][] }>();
+defineEmits<{
+  (event: "click", tile: [number, number]): any;
 }>();
-
-function computeTiles() {
-  let colors: { [state in FieldState]: string } = {
-    Empty: "red",
-    Miss: "blue",
-    Hit: "black",
-    Sunk: "green",
-  };
-
-  let tiles: { text: string; backgroundColor: string; onClick: () => any }[] = [];
-
-  for (let i = 0; i < 11; i++) {
-    for (let j = 0; j < 11; j++) {
-      if (i == 0) {
-        tiles.push({ text: j.toString(), backgroundColor: "black", onClick: () => {} });
-      } else if (j == 0) {
-        tiles.push({ text: "ABCDEFGHIJ"[i - 1], backgroundColor: "black", onClick: () => {} });
-      } else {
-        tiles.push({
-          text: `${i}.${j}`,
-          backgroundColor: colors[props.state.board[i - 1][j - 1]],
-          onClick: () => emit("select", [i - 1, j - 1]),
-        });
-      }
-    }
-  }
-
-  return tiles;
-}
 </script>
 
 <template>
-  <ChoiceDialog
-    v-if="state.selectedTile != null"
-    title="Tile type"
-    :options="[
-      ['Miss', () => $emit('Miss', state.selectedTile)],
-      ['Hit', () => $emit('Hit', state.selectedTile)],
-      ['Sunk', () => $emit('Sunk', state.selectedTile)],
-    ]"
-  />
   <div class="w-full h-full flex justify-center items-center">
     <div
       class="w-full h-full grid grid-cols-11 gap-1"
       style="max-width: min(70vw, 70vh); max-height: min(70vw, 70vh)"
     >
-      <div
-        v-for="(tile, index) in computeTiles()"
-        :key="index"
-        :style="{ color: 'white', backgroundColor: tile.backgroundColor }"
-        @click="tile.onClick"
-      >
-        {{ tile.text }}
-      </div>
+      <template v-for="(n1, i) in 11">
+        <template v-for="(n2, j) in 11">
+          <div v-if="i == 0 && j == 0" :key="i * j"></div>
+          <div v-if="i == 0 && j != 0" :key="i * j">{{ j }}</div>
+          <div v-if="i != 0 && j == 0" :key="i * j">
+            {{ "ABCDEFGHIJ"[i - 1] }}
+          </div>
+
+          <div
+            v-if="i != 0 && j != 0"
+            :key="i * j"
+            class="text-white"
+            :style="{ backgroundColor: tiles[i - 1][j - 1].color }"
+            @click="$emit('click', [i - 1, j - 1])"
+          >
+            {{ i }}.{{ j }}
+          </div>
+        </template>
+      </template>
     </div>
   </div>
 </template>
